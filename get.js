f2724e478fa0f4e8cf0bde6db8ade18e24962109
khai96_ <hvksmr1996@gmail.com>
2017-04-11 10:07:12 +0700

    Use lib/utils.js

--- get.js
+++ get.js
@@ -5,12 +5,12 @@ const {resolve} = require('path')
 const {readFileSync} = require('fs')
 const {argv} = require('process')
 const getStdIn = require('get-stdin')
-const {success, failure, getprop} = require('./lib/utils.js')
+const {success, failure, getproppath} = require('./lib/utils.js')
 const {parse, stringify} = JSON
 const usefulargv = argv.slice(2)
 
 const main = (json, field) =>
-  success(stringify(field.reduce(getprop, parse(json)), undefined, 2))
+  success(stringify(getproppath(parse(json), ...field), undefined, 2))
 
 getStdIn()
   .then(
